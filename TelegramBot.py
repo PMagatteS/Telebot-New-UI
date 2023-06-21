bdb1ebf66f51973f63306c4510317486965105e3
PMagatteS <papamagattesady@gmail.com>
2023-06-21 22:31:56 +0000

    First method to get the bot datas

--- TelegramBot.py
+++ TelegramBot.py
@@ -12,3 +12,10 @@ class TelegramBot():
         self.serverCooldown = 2 if not kw.get("serverCooldown") else kw.get("serverCooldown")
         self.apiUrl = "https://api.telegram.org/bot"
         self.serverRunning = False
+
+    def getMe(self, **kw):
+        if kw.get("token"):
+            res = json.loads(requests.post(f"{self.apiUrl+kw.get('token')}/getMe", timeout=5).content)
+            return res
+        res = json.loads(requests.post(f"{self.apiUrl+self.botToken}/getMe").content)
+        return res
